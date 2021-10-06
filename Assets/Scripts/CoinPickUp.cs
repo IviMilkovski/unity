a714c5a09e390e6363565f66245fecb38c940872
Iva Milkovski <ivamilkovski44@gmail.com>
2021-10-06 22:18:43 +0200

    Add files via upload

---
+++ Assets/Scripts/CoinPickUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinPickUp : MonoBehaviour
+{
+    [SerializeField] AudioClip coinPickUpSFX;
+    [SerializeField] int pointsForCoinPickup = 100;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        FindObjectOfType<GameSession>().AddToScore(pointsForCoinPickup);
+        AudioSource.PlayClipAtPoint(coinPickUpSFX, Camera.main.transform.position);
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] Vector2 deathKick = new Vector2(2f, 2f);
+
+    bool isAlive = true;
+
+    Rigidbody2D myRigidBody;
+    Animator myAnimator;
+    CapsuleCollider2D myBodyCollider;
+    BoxCollider2D myFeet;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myBodyCollider = GetComponent<CapsuleCollider2D>();
+        myFeet = GetComponent<BoxCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isAlive) { return; }
+
+        Run();
+        Jump();
+        FlipSprite();
+        Die();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal"); // value is betweeen -1 to +1
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("Run", playerHasHorizontalSpeed);
+
+    }
+
+    private void Jump()
+    {
+        if (!myFeet.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+
+
+        if (CrossPlatformInputManager.GetButtonDown("Jump"))
+        {
+            Vector2 jumpVelocityToAdd = new Vector2(0f, jumpSpeed);
+            myRigidBody.velocity += jumpVelocityToAdd;
+        }
+    }
+    private void Die()
+    {
+        if (myBodyCollider.IsTouchingLayers(LayerMask.GetMask("WaterDan")))
+        {
+            isAlive = false;
+            myAnimator.SetTrigger("Dying");
+            GetComponent<Rigidbody2D>().velocity = deathKick;
+            FindObjectOfType<GameSession>().ProcessPlayerDeath();
+        }
+    }
+
+    private void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(myRigidBody.velocity.x), 1f);
+        }
+    }
+
+}
